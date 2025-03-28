15562b38055028b081e6b8325aab072fd5b38469
Eustia232 <a728076618@163.com>
2025-03-28 13:01:28 +0800

    commit

--- src/record.py
+++ src/record.py
@@ -10,15 +10,24 @@ def record(id, title):
     :return:
     """
 
+    # 判断文件是否存在
     if os.path.exists('../status/already.json'):
+        # 如果存在，则打开文件，读取内容
         with open('../status/already.json', "r", encoding='utf-8') as json_file:
             rec_list = json.load(json_file)
             already = set(rec_list)
     else:
+        # 如果不存在，则创建一个空集合
         already = set()
+    # 将id和title拼接成一个字符串，添加到集合中
     already.add(f'{id}:{title}')
+    # 将集合转换为列表
+    already = list(already)
+    #对already进行排序
+    already.sort(key=lambda x: int(x.split(':')[0]))
+    # 将列表写入文件
     with open('../status/already.json', 'w', encoding='utf-8') as json_file:
-        json.dump(list(already), json_file, ensure_ascii=False, indent=4)
+        json.dump(already, json_file, ensure_ascii=False, indent=4)
 
 
 if __name__ == '__main__':
